04f7225fdc948e98479f9a162b67386fce094219
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-07-13 12:34:20 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -36,7 +36,7 @@ object A : BuildType({
 
 object BuildConfig1 : BuildType({
     name = "buildConfig1"
-    this.id = RelativeId("${id76799.relativeId}_Build")
+    this.id = RelativeId("${project.relativeId}_Build")
 })
 
 
